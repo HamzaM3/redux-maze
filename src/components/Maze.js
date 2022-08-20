451377f5bf9e13efc0e0b69ff2bf92e2e4cd54b7
HamzaM3 <99541093+HamzaM3@users.noreply.github.com>
2022-08-20 11:12:56 +0200

    Walls done

--- src/components/Maze.js
+++ src/components/Maze.js
@@ -2,31 +2,75 @@ import MazeLogic from '../maze-logic/Maze'
 import Position from '../maze-logic/Position'
 import { useState } from 'react'
 
+function topTest(m, pos) {
+    return !m.checkLink(pos.up(), pos)
+}
+
+function leftTest(m, pos) {
+    return !m.checkLink(pos.left(), pos);
+}
+
+function rightTest(m, pos) {
+    return !m.checkLink(pos, pos.right());
+}
+
+function bottomTest(m, pos) {
+    return !m.checkLink(pos, pos.down())
+}
+
 export default function Maze ({n}) {
     let [m, setMaze] = useState(new MazeLogic(n))
 
-    console.log(m)
     let res = [];
-    for (let j = 0; j < n; j++) {
-        for (let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
             let pos = new Position(n, i, j)
 
-            console.log(pos.up(), pos, m.checkLink(pos.up(), pos))
-            console.log(pos.left(), pos, m.checkLink(pos.left(), pos))
-            console.log(pos.right(), pos, m.checkLink(pos, pos.right()))
-            console.log(pos.down(), pos, m.checkLink(pos, pos.down()))
-
-            res.push(<div key={"(" + i + "," + j + ")"} 
-                        style = {{
-                            borderTop: !m.checkLink(pos.up(), pos) ? "3px solid black" : null,
-                            borderLeft: !m.checkLink(pos.left(), pos) ? "3px solid black" : null,
-                            borderRight: !m.checkLink(pos, pos.right()) ? "3px solid black" : null,
-                            borderBottom: !m.checkLink(pos, pos.down()) ? "3px solid black" : null
-                        }}></div>)
+            if (topTest(m, pos)) {
+                res.push(<div className="horizontal-wall" key={`wall-horiz-${i}-${j}`} style={{
+                                                                    gridRow: 2 * j + 1,
+                                                                    gridColumn: `${2 * i + 1} / span 3`,
+                                                                    zIndex: i*n + j}}>
+                    <div className='top'></div>
+                    <div className='front'></div>
+                </div>)
+            }
+            
+            if (leftTest(m, pos)) {
+                res.push(<div className="vertical-wall" key={`wall-vert-${i}-${j}`} style={{
+                                                                    gridRow: `${2 * j + 1} / span 3`,
+                                                                    gridColumn: 2 * i + 1, 
+                                                                    zIndex: i*n + j}}>
+                    <div className='top'></div>
+                    <div className='front'></div>
+                </div>)
+            }
+            
         }
     }
 
-    return <div className="maze" style={{gridTemplateColumns: `repeat(${n}, 1fr)`}}>
+    for (let i = 0; i < n; i++) {
+        res.push(
+            <div className="vertical-wall" key={`vertical-${i}`} style={{gridRow: `${2 * i + 1} / span 3`, gridColumn: 2 * n + 1, zIndex: 100000}}>
+                <div className='top'></div>
+                <div className='front'></div>
+            </div>
+        )
+        res.push(
+            <div className="horizontal-wall" key={`horizontal-${i}`} style={{gridRow: 2 * n + 1, gridColumn: `${2 * i + 1} / span 3`, zIndex: 100002}}>
+                <div className='top'></div>
+                <div className='front'></div>
+            </div>
+        )
+    }
+    
+    
+    
+
+    return <div className="maze" style={{
+        gridTemplateColumns: `10px repeat(${n}, 70px 10px)`,
+        gridTemplateRows: `30px repeat(${n}, 50px 30px)`
+    }}>
         {res}
     </div>
 }
